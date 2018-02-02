ddac742c03b0e920b7bd76318e3c2804eb8a8fa4
David Kalu <davidpraise45@gmail.com>
2018-02-02 10:04:18 +0100

    Add files via upload

---
+++ StudentAdmission.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentadmission;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author david
+ */
+public class StudentAdmission extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        Application a = new Application();
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        a.setExtendedState(JFrame.MAXIMIZED_HORIZ);
+        a.setVisible(true);
+        a.setSize(680, 835);
+     
+    }
+    
+}
